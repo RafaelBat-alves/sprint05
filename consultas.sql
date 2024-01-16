a3f36402e24f5a0c923ff10633d59721420f324d
RAFAEL <rafaelolivais09@gmail.com>
2024-01-16 13:56:21 -0300

    3 feito

--- consultas.sql
+++ consultas.sql
@@ -1,1 +1,3 @@
-select * from categoria,produto;
+select * from categoria a inner join produto b on a.id = b.id order by a.nome;
+select * from categoria a inner join produto b on a.id = b.id order by a.nome;
+select a.id,a.nome,b.id,b.nome,b.preco from categoria a inner join produto b on a.id = b.id order by a.nome;
